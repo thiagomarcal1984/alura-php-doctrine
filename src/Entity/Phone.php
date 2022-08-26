8662a8a318a683aa7c1dda815fe2f4248dcbbea8
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-08-26 15:54:56 -0300

    Mapeando as novidades.

--- src/Entity/Phone.php
+++ src/Entity/Phone.php
@@ -6,16 +6,26 @@ use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\GeneratedValue;
 use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\ManyToOne;
 
 #[Entity]
 class Phone
 {
     #[Id, GeneratedValue, Column]
     public int $id;
+
+    #[ManyToOne(targetEntity: Student::class, inversedBy: "phones")] // inversedBy é opcional.
+    public readonly Student $student;
+
     public function __construct(
         #[Column]
         public readonly string $number
     )
     {
+    }
+
+    public function setStudent(Student $student): void
+    {
+        $this->student = $student;
     }
 }
--- src/Entity/Student.php
+++ src/Entity/Student.php
@@ -2,18 +2,21 @@
 
 namespace Alura\Doctrine\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\GeneratedValue;
 use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\OneToMany;
 
 #[Entity]
 class Student
 {    
-    #[Id]
-    #[GeneratedValue]
-    #[Column]
+    #[Id, GeneratedValue, Column]
     public int $id; // O Doctrine prcisa que a id esteja acessível.
+
+    #[OneToMany(targetEntity: Phone::class, mappedBy: "student")]
+    private iterable $phones; // Este objeto não é um array nativo do PHP.
     
     // Sintaxe de construtor nova no PHP 8. Se chama Promoção de Propriedades.
     public function __construct(
@@ -23,6 +26,19 @@ class Student
         // esta propriedade permita escrita.
     )
     {
-        
+        $this->phones = new ArrayCollection();
+    }
+
+    public function addPhone(Phone $phone): void
+    {
+        // $this->phones->add($phone); // Sintaxe possível, se $phones fosse 
+        // da classe Collection do Doctrine.
+        $this->phones[] = $phone; // Este objeto não é um array nativo do PHP.
+        $phone->setStudent($this);
+    }
+
+    public function phones(): iterable
+    {
+        return $this->phones;
     }
 }
