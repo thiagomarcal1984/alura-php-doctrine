218ad0138a0896be940b7f982f8b073f6aee01e3
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-08-26 14:18:33 -0300

    Buscando os alunos.

--- bin/insert-student.php
+++ bin/insert-student.php
@@ -6,8 +6,8 @@ use Alura\Doctrine\Helper\EntityManagerCreator;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $entityManager = EntityManagerCreator::createEntityManager();
-
-$student = new Student('Vinícius Dias');
+$student = new Student($argv[1]); // $argv[1] significa o primeiro parâmetro ao usar o script na linha de comando.
+// $argv[0] é o caminho para o arquivo do script PHP: neste caso, .\bin\insert-student.php.
 
 $entityManager->persist($student); 
 // O método persist apenas guarda o objeto no EntityManager, não no database.
---
+++ bin/list-student.php
@@ -0,0 +1,36 @@
+<?php
+
+use Alura\Doctrine\Entity\Student;
+use Alura\Doctrine\Helper\EntityManagerCreator;
+
+require_once __DIR__ . "/../vendor/autoload.php";
+
+$entityManager = EntityManagerCreator::createEntityManager();
+$studentRepository = $entityManager->getRepository(Student::class);
+
+// Este comentário a seguir serve para ajudar a IDE a identificar os objetos dentro do array retornado.
+/** @var Student[] $studentList */
+$studentList = $studentRepository->findAll();
+
+foreach ($studentList as $student) {
+    echo "ID: $student->id\nNome: $student->name\n\n";
+}
+
+// Para buscar um único objeto pelo ID, use find.
+// De novo: o comentário abaixo serve para facilitar o autocomplete da IDE.
+/** @var Student $alunoTeste */
+$alunoTeste = $studentRepository->find(2);
+echo $alunoTeste->name;
+
+// Para filtrar o repositório, use um array com chave/valor com o método findBy:
+echo PHP_EOL . "Usando o método findBy:" . PHP_EOL;
+$result = $studentRepository->findBy(['name' => "Maria José"]);
+var_dump($result);
+
+// O método findBy retorna um array; já o método findOneBy retorna um único objeto:
+echo PHP_EOL . "Usando o método findOneBy:" . PHP_EOL;
+$result = $studentRepository->findOneBy(['name'=> "João da Silva"]);
+var_dump($result);
+
+// Para contar elementos, use o método count (e forneça algum array, mesmo que vazio).
+echo PHP_EOL . "Número de Students: " . $studentRepository->count([]) . PHP_EOL;
